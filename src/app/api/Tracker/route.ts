83001ab59bf47d2c48d7e840a2e80237aa29090c
jeet <gjeet7415@gmail.com>
2025-07-12 11:40:27 +0530

    .

--- src/app/api/Tracker/route.ts
+++ src/app/api/Tracker/route.ts
@@ -126,7 +126,7 @@ export async function GET(req: NextRequest) {
     }
 
    function fetchWidgetsAndRender() {
-  fetch(\`http://localhost:3000/api/sites/\${siteId}/Widgets\`)
+  fetch(\`https://proofly-delta.vercel.app/api/sites/\${siteId}/Widgets\`)
     .then(res => res.json())
     .then(widgets => {
       widgets.forEach(widget => {
