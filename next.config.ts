6b0a5d2dcb49162a4e478a00d52518be80cbd0cf
jeet <gjeet7415@gmail.com>
2025-07-14 18:29:15 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output : "standalone",
   images: {
     domains: ['uploads-ssl.webflow.com', 'lh3.googleusercontent.com', 'images.remotePatterns'],
   },
